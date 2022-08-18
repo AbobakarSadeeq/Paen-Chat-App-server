9062c808a21a41b0b9010455ec4d26f5ca6ee536
Abobakar Sadeeq <abobakarkhan7@gmail.com>
2022-08-18 15:55:33 +0500

    project server side of chat
    added done

---
+++ DataAccess/DataContext Class/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DataContext_Class
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Test> Tests { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration<Test>(new TestConfiguration());
+        }
+
+
+    }
+}
---
+++ paen-chat-app-server/Program.cs
@@ -0,0 +1,52 @@
+using DataAccess.DataContext_Class;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContextPool<DataContext>(options =>
+                options.UseSqlServer(
+                builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// connecting client
+builder.Services.AddCors(opt => {
+    opt.AddDefaultPolicy(builder => {
+        builder.AllowAnyOrigin();
+        builder.AllowAnyMethod();
+        builder.AllowAnyHeader();
+    });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseCors(a => {
+    a.WithOrigins(builder.Configuration.GetValue<string>("Client_URL"))
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+});
+
+app.UseRouting();
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
